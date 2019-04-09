ed450257cf54206c64af9e07c2b01edebf1efe29
Peter Kim <kroits09@gmail.com>
2019-04-09 09:53:17 +0900

    Add Home and EditAccount

---
+++ src/sharedQueries.ts
@@ -0,0 +1,18 @@
+import { gql } from "apollo-boost";
+
+export const USER_PROFILE = gql`
+  query userProfile {
+    GetMyProfile {
+      ok
+      error
+      user {
+        profilePhoto
+        firstName
+        lastName
+        email
+        fullName
+        isDriving
+      }
+    }
+  }
+`;
--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -3,6 +3,49 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: toggleDriving
+// ====================================================
+
+export interface toggleDriving_ToggleDrivingMode {
+  __typename: "ToggleDrivingModeResponse";
+  ok: boolean;
+  error: string | null;
+}
+
+export interface toggleDriving {
+  ToggleDrivingMode: toggleDriving_ToggleDrivingMode;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: updateProfile
+// ====================================================
+
+export interface updateProfile_UpdateMyProfile {
+  __typename: "UpdateMyProfileResponse";
+  ok: boolean;
+  error: string | null;
+}
+
+export interface updateProfile {
+  UpdateMyProfile: updateProfile_UpdateMyProfile;
+}
+
+export interface updateProfileVariables {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePhoto: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: startPhoneVerification
 // ====================================================
 
@@ -68,6 +111,35 @@ export interface verifyPhone {
 export interface verifyPhoneVariables {
   key: string;
   phoneNumber: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: userProfile
+// ====================================================
+
+export interface userProfile_GetMyProfile_user {
+  __typename: "User";
+  profilePhoto: string | null;
+  firstName: string;
+  lastName: string;
+  email: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+}
+
+export interface userProfile_GetMyProfile {
+  __typename: "GetMyProfileResponse";
+  ok: boolean;
+  error: string | null;
+  user: userProfile_GetMyProfile_user | null;
+}
+
+export interface userProfile {
+  GetMyProfile: userProfile_GetMyProfile;
 }
 
 /* tslint:disable */
